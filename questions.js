7239078571f9028c5c25d565f39d32144bc2af0a
muncheng <munchythemantou@gmail.com>
2026-06-18 08:20:35 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,60 @@
+const questions = [
+{
+category: "programming",
+questions: [
+
+{
+audio: "audio/bluewhale1.mp3",
+explanation: "This is a blue whale call, known for its deep, gutteral pitch",
+question: "Is this a blue whale?",
+options: [
+"Blue Whale",
+"Not Blue Whale"
+],
+correctAnswer: 0
+},
+
+{
+audio: "audio/humpback1.mp3",
+explanation: "This is a humpback whale call, they are known for having a song-like pitch and high thrill",
+question: "Is this a blue whale?",
+options: [
+"Blue Whale",
+"Not Blue Whale"
+],
+correctAnswer: 1
+},
+{
+audio: "audio/bluewhale2.mp3",
+explanation: "This is a blue whale call recorded in the Northeast Pacfic Ocean",
+question: "Is this a blue whale?",
+options: [
+"Blue Whale",
+"Not Blue Whale"
+],
+correctAnswer: 0
+},
+{
+audio: "audio/ship1.mp3",
+explanation: "This is the sound of a seal!",
+question: "Is this a blue whale?",
+options: [
+"Blue Whale",
+"Not Blue Whale"
+],
+correctAnswer: 1
+},
+{
+audio: "audio/bluewhale3.mp3",
+explanation: "This is a blue whale call recorded in deep sea, the sound has been altered so the frequency can be heard by humans.",
+question: "Is this a blue whale?",
+options: [
+"Blue Whale",
+"Not Blue Whale"
+],
+correctAnswer: 0
+},
+
+]
+}
+];
